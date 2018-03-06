fd517cfa6cbd267980b0013e60d0f35ded8f40a0
Moon Antonio <pinchodev91@gmail.com>
2018-03-06 18:33:00 +0100

    Funcionalidad basica

--- BehaviourScenes Project/Assets/Proyecto BehaviourScenes/Scripts/BehaviourScenes.cs
+++ BehaviourScenes Project/Assets/Proyecto BehaviourScenes/Scripts/BehaviourScenes.cs
@@ -1,16 +1,58 @@
-﻿using System.Collections;
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// BehaviourScenes.cs (06/03/2018)												\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Control de la transicion de escena unica					\\
+// Fecha Mod:		06/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#endregion
 
-public class BehaviourScenes : MonoBehaviour {
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Control de la transicion de escena unica.</para>
+	/// </summary>
+	public class BehaviourScenes : MonoBehaviour
+	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Escena que cargara al inicio.</para>
+		/// </summary>
+		public string escenaInicial = string.Empty;						// Escena que cargara al inicio
+		#endregion
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Todas las escenas de la escena.</para>
+		/// </summary>
+		private GameObject[] escenas;                                   // Todas las escenas de la escena
+		#endregion
+
+		#region Inicializadores
+		/// <summary>
+		/// <para>Inicializador de <see cref="BehaviourScenes"/>.</para>
+		/// </summary>
+		private void Start()// Inicializador de BehaviourScenes
+		{
+			// Agregar todas las escenas de la escena
+			escenas = GameObject.FindGameObjectsWithTag("Transicion");
+
+			// Fija la primera escena
+			SetEscena(escenaInicial);
+		}
+		#endregion
+
+		#region API
+		public void SetEscena(string escena)
+		{
+
+		}
+		#endregion
 	}
 }
