64261baeebd27b1c7e8a877e6fb4067730bbfaf9
Moon Antonio <pinchodev91@gmail.com>
2018-03-06 18:45:12 +0100

    Funcionalidad transicion finalizada

--- BehaviourScenes Project/Assets/Proyecto BehaviourScenes/Scripts/BehaviourScenes.cs
+++ BehaviourScenes Project/Assets/Proyecto BehaviourScenes/Scripts/BehaviourScenes.cs
@@ -9,7 +9,6 @@
 
 #region Librerias
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 #endregion
 
@@ -49,9 +48,53 @@ namespace MoonAntonio
 		#endregion
 
 		#region API
-		public void SetEscena(string escena)
+		/// <summary>
+		/// <para>Cambia la escena a la dada.</para>
+		/// </summary>
+		/// <param name="escena">Nueva escena.</param>
+		public void SetEscena(string escena)// Cambia la escena a la dada
 		{
+			StartCoroutine(CambiarEscena(escena));
+		}
+		#endregion
 
+		#region Corrutina
+		/// <summary>
+		/// <para>Procesamiento</para>
+		/// </summary>
+		/// <param name="newEscena"></param>
+		/// <returns></returns>
+		public IEnumerator CambiarEscena(string newEscena)// Procesamiento
+		{
+			// Bloquear inputs
+			gameObject.GetComponent<CanvasGroup>().interactable = false;
+
+			// Ocultar
+			Animator animator = gameObject.GetComponent<Animator>();
+			animator.SetBool("Hide", true);
+
+			// Esperar a que la animacion termine
+			yield return new WaitForSeconds(1);
+
+			// Ocultar todas las escenas
+			foreach (GameObject escena in escenas)
+			{
+				escena.GetComponent<CanvasGroup>().alpha = 0;
+				escena.GetComponent<CanvasGroup>().interactable = false;
+				escena.GetComponent<CanvasGroup>().blocksRaycasts = false;
+			}
+
+			// Activar la nueva escena
+			GameObject escenaActiva = GameObject.Find(newEscena);
+			escenaActiva.GetComponent<CanvasGroup>().alpha = 1;
+			escenaActiva.GetComponent<CanvasGroup>().interactable = true;
+			escenaActiva.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+			// Mostrar canvas
+			animator.SetBool("Hide", false);
+
+			// Habilitar inputs
+			gameObject.GetComponent<CanvasGroup>().interactable = true;
 		}
 		#endregion
 	}
